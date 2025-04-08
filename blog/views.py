654dc95cdcc58de56d582191e6121deffc7ca074
lizagqq <lisaverevkina2003@gmail.com>
2025-04-08 21:30:08 +0300

    Updated admin, models, views, urls, and added new migration

--- blog/views.py
+++ blog/views.py
@@ -1,35 +1,39 @@
-from django.shortcuts import render, get_object_or_404
-from django.http import Http404
+from django.shortcuts import get_object_or_404, render
 from django.utils import timezone
-from .models import Post, Category
+from blog.models import Post, Category
+from django.conf import settings
 
 def index(request):
-    posts = Post.objects.filter(
-        pub_date__lte=timezone.now(),
+    template = 'blog/index.html'
+    post_list = Post.objects.filter(
         is_published=True,
-        category__is_published=True
-    ).order_by('-pub_date')[:5]
-    return render(request, 'blog/index.html', {'post_list': posts})
+        category__is_published=True,
+        pub_date__lte=timezone.now()
+    ).order_by('-pub_date')[:settings.POSTS_PER_PAGE]
+
+    context = {'post_list': post_list}
+    return render(request, template, context)
+
 
 def post_detail(request, pk):
-    post = get_object_or_404(Post, pk=pk)
-    if (post.pub_date > timezone.now() or 
-        not post.is_published or 
-        not post.category.is_published):
-        raise Http404
-    return render(request, 'blog/detail.html', {'post': post})
+    template = 'blog/detail.html'
+    post = get_object_or_404(Post,
+                             pub_date__lte=timezone.now(),
+                             is_published=True,
+                             category__is_published=True,
+                             id=pk)
+    context = {'post': post}
+    return render(request, template, context)
 
-def category_posts(request, slug):
-    category = get_object_or_404(Category, slug=slug, is_published=True)
-    posts = Post.objects.filter(
-        category=category,
+
+def category_posts(request, category_slug):
+    template = 'blog/category.html'
+    category = get_object_or_404(Category, is_published=True,
+                                 slug=category_slug)
+    post_list = category.posts.filter(
         is_published=True,
         pub_date__lte=timezone.now()
-    ).order_by('-pub_date')
-    return render(request, 'blog/category.html', {'category': category, 'post_list': posts})
-
-def about(request):
-    return render(request, 'blog/about.html')
-
-def rules(request):
-    return render(request, 'blog/rules.html')
+    )
+    context = {'category': category,
+               'post_list': post_list}
+    return render(request, template, context)
